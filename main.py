6539f7dada39059ffd221b3c68b9ec9ae0d6070a
Drew Erny <dperny@crimson.ua.edu>
2013-04-21 22:39:47 -0500

    completed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from peekablefile import *
 import sys
 import string
+from stack import LinkedListStack as stack
+from typetoken import Typed_Token
 SPEC_CHARS = ['_']
 
 def read_real(f):
@@ -26,13 +28,16 @@ def main(argv):
 
     f = PeekableFile(argv[1])
     c = f.peek(1)
+    store = stack()
     while c:
         if c in string.digits:
             num = read_real(f)
             if isinstance(num,int):
-                print('Integer:',num)
+                # print('Integer:',num)
+                pass
             elif isinstance(num,float):
-                print('Float:',num)
+                # print('Float:',num)
+                pass
         elif c == '<':
             f.read(1)
             c = f.peek(1)
@@ -41,22 +46,49 @@ def main(argv):
                 chars = read_string(f)
                 c = f.peek(1)
                 if c == '>':
-                    print('EndTag:',chars)
+                    try:
+                        start = store.pop()
+                    except IndexError:
+                        # if there's nothing on the stack and we have an end tag, error and break
+                        print("UnexpectedEndTagError: Unexpected </{0}> found!".format(chars))
+                        return
+                    # if the end tags doesn't match with the start, error out and break
+                    if start.value != chars:
+                        print("WrongEndTagError: Expected </{0}> but found </{1}>!".format(start.value,chars))
+                        return
+                    # print('EndTag:',chars)
                 else:
-                    print('String:',chars)
+                    # print('String:',chars)
+                    pass
             else:
                 chars = read_string(f)
                 c = f.peek(1)
                 if c == '>':
-                    print('StartTag:',chars)
+                    # this thing is a StartTag, put it on the stack
+                    store.push(Typed_Token('StartTag',chars))
+                    # print('StartTag:',chars)
                 else:
-                    print('String:',chars)
+                    pass
+                    # print('String:',chars)
         elif c in string.ascii_letters:
             chars = read_string(f)
-            print('String:', chars)
+            # print('String:', chars)
         else:
             f.read(1) # discard the character
         c = f.peek(1)
+
+    try:
+        # grab anything left on the stack, if not empty it's an error
+        end = store.pop()
+        if end.kind == 'StartTag':
+            print("MissingEndTagError: Expected </{0}> not found!".format(end.value))
+        else:
+            print("UnexpectedEndTagError: Unexpected </{0}> found!".format(end.value))
+
+    except IndexError:
+        #pass silently, nothing left in the stack
+        pass
+
 
 if __name__ == '__main__':
     main(sys.argv)
--- typetoken.py
+++ typetoken.py
@@ -5,3 +5,9 @@ class Typed_Token:
 
     def __str__(self):
         return "({0}, {1})".format(self.kind,self.value)
+    
+    def __eq__(self,other):
+        return self.value == other.value
+    
+    def __ne__(self,other):
+        return self.value != other.value
